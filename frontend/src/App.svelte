920405576a4f2410dbeafea93191c04a8a128269
Kim Jeong-won <threedalpeng@gmail.com>
2022-11-06 23:46:59 +0900

    Create Test Page

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Layout from "./components/layout/Layout.svelte";
+  import TestPage from "./pages/Test/TestPage.svelte";
 </script>
 
-<Layout />
+<Layout><TestPage /></Layout>
