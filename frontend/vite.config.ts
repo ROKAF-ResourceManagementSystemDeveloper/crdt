7f294331e64b743343ef8b45235ec3f20d694f9e
Kim Jeong-won <threedalpeng@gmail.com>
2022-11-06 23:40:04 +0900

    Seperate unocss config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import Unocss from "unocss/vite";
+import unocss from "./uno.config";
 import { extractorSvelte } from "@unocss/core";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Unocss({ extractors: [extractorSvelte] }), svelte()],
+  plugins: [Unocss(unocss), svelte()],
   server: {
     host: "0.0.0.0",
     port: 5053,
